a30ae770b12764646d16b46c018621f401d65ba9
supergumby <gcleris@gmail.com>
2018-08-14 20:52:38 -0400

    Deuxième commit pour ajouter la classe Item.java.

---
+++ GroceryList/src/Item.java
@@ -0,0 +1,5 @@
+// Classe servant à la construction d'items de la liste d'épicérie.
+
+public class Item {
+
+}
--- GroceryList/src/List.java
+++ GroceryList/src/List.java
@@ -6,7 +6,7 @@ public class List {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Le programme principal est écrit ici.
 	}
 
 }
